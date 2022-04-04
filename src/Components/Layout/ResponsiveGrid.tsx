0d9db62cd16547302134e5bcb6b866502d59a147
Tomasz <skretkowskitomasz@gmail.com>
2022-04-04 23:20:31 +0200

    SPAR-13 Commented problematic code

--- src/Components/Layout/ResponsiveGrid.tsx
+++ src/Components/Layout/ResponsiveGrid.tsx
@@ -5,13 +5,13 @@ import { experimentalStyled as styled } from '@mui/material/styles';
 
 import ServiceCard from '../Organisms/ServiceCard/ServiceCard';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(2),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
 
 export default function ResponsiveGrid() {
   return (
