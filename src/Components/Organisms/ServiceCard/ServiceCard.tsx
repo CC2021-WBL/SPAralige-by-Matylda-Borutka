a9d2489f4e445bd3038900a2e8f9657c5878a894
Tomasz <skretkowskitomasz@gmail.com>
2022-04-06 01:16:43 +0200

    SPAR-13    #Added:       useStyles Hook for styling,       service mock object,       component made with reusability purpose;
          MUIstyles  and React i18n packages;

--- src/Components/Organisms/ServiceCard/ServiceCard.tsx
+++ src/Components/Organisms/ServiceCard/ServiceCard.tsx
@@ -1,3 +1,5 @@
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -5,36 +7,99 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CreateIcon from '@mui/icons-material/Create';
 import Typography from '@mui/material/Typography';
+import { Stack } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  btnsPosition: {
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 10,
+    '@media screen and (min-width: 600px)': {
+      justifyContent: 'flex-end',
+    },
+  },
+  btnStyles: {
+    borderRadius: 50,
+    minWidth: 200,
+    paddingTop: 9,
+    '@media screen and (min-width: 450px)': {
+      '&:first-child': {
+        marginRight: 8,
+      },
+    },
+  },
+  durationAndPrice: {
+    alignItems: 'center',
+    paddingBottom: 10,
+  },
+});
 
 const ServiceCard = () => {
+  const classes = useStyles();
+
+  const service = {
+    name: 'Masaż Gorącymi Kamieniami',
+    description:
+      'Odpręż się chłopie, to tutaj znajdziesz ukojenie. Zapomnij o troskach, zapomnij o brzemieniu.. Połóż się i nie myśl o niczym. Odpocznij,',
+    price: 150,
+    duration: 1.5,
+    imageUrl: require('../../../Assets/massage.jpg'),
+    altText: 'massage with stones',
+  };
+
   return (
     <Card>
       <CardMedia
         component="img"
         height="210"
-        image={require('../../../Assets/massage.jpg')}
-        alt="massage with stones"
+        image={service.imageUrl}
+        alt={service.altText}
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Masaż Gorącymi Kamieniami
+        <Typography gutterBottom variant="h6" component="div">
+          {service.name}
         </Typography>
+        <Stack direction="row" spacing={2}>
+          <Stack
+            spacing={0.8}
+            direction="row"
+            className={classes.durationAndPrice}
+          >
+            <AccessTimeIcon sx={{ color: 'text.disabled' }} />
+            <Typography
+              variant="subtitle2"
+              component="div"
+              color="text.disabled"
+            >
+              {`${service.duration} h`}
+            </Typography>
+          </Stack>
+          <Stack
+            spacing={0.4}
+            direction="row"
+            className={classes.durationAndPrice}
+          >
+            <AttachMoneyIcon sx={{ color: 'text.disabled' }} />
+            <Typography
+              variant="subtitle2"
+              component="div"
+              color="text.disabled"
+            >
+              {`${service.price} zł`}
+            </Typography>
+          </Stack>
+        </Stack>
         <Typography variant="body2" color="text.secondary">
-          Odpręż się chłopie, to tutaj znajdziesz ukojenie. Zapomnij o troskach,
-          zapomnij o brzemieniu.. Połóż się i nie myśl o niczym. Odpocznij,
+          {service.description}
         </Typography>
       </CardContent>
-      <CardActions
-        sx={{
-          flexWrap: 'wrap',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
+      <CardActions className={classes.btnsPosition} disableSpacing>
         <Button
           aria-label="more details"
           size="medium"
-          sx={{ borderRadius: 50, minWidth: 200, paddingTop: '9px' }}
+          className={classes.btnStyles}
           onClick={() => {
             console.log('clicked');
           }}
@@ -44,13 +109,10 @@ const ServiceCard = () => {
         <Button
           aria-label="make reservation"
           variant="contained"
+          disableElevation
           size="medium"
           startIcon={<CreateIcon fontSize="small" />}
-          sx={{
-            borderRadius: 50,
-            minWidth: 200,
-            paddingTop: '9px',
-          }}
+          className={classes.btnStyles}
           onClick={() => {
             console.log('clicked');
           }}
