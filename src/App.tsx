a58181d0b4aba76ae8c0f1f0ae504f5563e7f74c
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-04-05 12:00:42 +0200

    Created routes for pages: about, account, reservations, treatments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 
+import AboutPage from './Components/Pages/AboutPage/AboutPage';
+import AccountPage from './Components/Pages/AccountPage/AccountPage';
 import LandingPage from './Components/Pages/LandingPage/LandingPage';
 import Layout from './Components/Layout/Layout';
+import ReservationsPage from './Components/Pages/ReservationsPage/ReservationsPage';
+import TreatmentsPage from './Components/Pages/TreatmentsPage/TreatmentsPage';
 
 function App() {
   return (
@@ -9,6 +13,14 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<LandingPage />} />
+
+          <Route path="/treatments" element={<TreatmentsPage />} />
+
+          <Route path="/about" element={<AboutPage />} />
+
+          <Route path="/account" element={<AccountPage />} />
+
+          <Route path="/reservations" element={<ReservationsPage />} />
         </Route>
       </Routes>
     </div>
---
+++ src/Components/Pages/AccountPage/AccountPage.tsx
@@ -0,0 +1,9 @@
+const AccountPage = () => {
+  return (
+    <main>
+      <h1>Tu będą informacje o ustawieniach konta</h1>
+    </main>
+  );
+};
+
+export default AccountPage;
