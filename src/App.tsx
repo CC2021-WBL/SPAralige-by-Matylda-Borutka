493365589f34faeedb9cb2d86f0df4dc62ee6058
Monika Krella <monika.krella@gmail.com>
2022-04-06 11:41:27 +0200

    Merge branch 'development' of https://github.com/CC2021-WBL/SPAralige-by-Matylda-Borutka into SPAR-15-login
    # Conflicts:
    #	src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 
 import LandingPage from './Components/Pages/LandingPage/LandingPage';
 import Layout from './Components/Layout/Layout';
-import LoginForm from './Components/Organisms/LoginForm/LoginForm';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginForm />} />
         </Route>
       </Routes>
     </div>
