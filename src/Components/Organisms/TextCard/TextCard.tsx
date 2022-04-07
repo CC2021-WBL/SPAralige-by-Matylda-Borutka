1b4acccccc575fcd228076a1a6db0fd4d728c0f9
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-04-07 19:35:12 +0200

    SPAR-43 Created text card

---
+++ src/Components/Organisms/TextCard/TextCard.tsx
@@ -0,0 +1,53 @@
+import Card from '@mui/material/Card';
+import { Button, CardActions, CardContent, CardHeader } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  card: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  title: {
+    margin: '0 0.5rem',
+    lineHeight: '2rem',
+  },
+  cardContent: {
+    fontSize: '0.875rem',
+  },
+  cardActions: {
+    justifyContent: 'center',
+    marginBottom: '1rem',
+  },
+});
+
+const TextCard = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        title="Sparalige By Matylda Borutka"
+        titleTypographyProps={{
+          fontSize: 20,
+        }}
+        className={classes.title}
+      />
+      <CardContent className={classes.cardContent}>
+        Nazywam się Matylda Borutka, założyłam własny gabinet 7 lat temu.
+        Sparalige to miejsce wyciszenia i, relaksacji i regeneracji fizycznej i
+        psychicznej. Stawiamy na indywidualne podejście i głębokie rozpoznanie
+        problemu pacjenta. Nie wiesz od czego zacząć? Skontaktuj się ze mną,
+        doradzę:
+      </CardContent>
+
+      <CardActions className={classes.cardActions}>
+        <Button variant="outlined">Napisz maila</Button>
+        <Button variant="outlined">Zadzwoń</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default TextCard;
