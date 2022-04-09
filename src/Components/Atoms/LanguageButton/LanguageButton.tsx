7da4915dc41c896a6c8c7989fcbbecc6bb317546
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-04-10 01:44:00 +0200

    Added and styled language buttons

---
+++ src/Components/Atoms/LanguageButton/LanguageButton.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@mui/material';
+import { ReactNode } from 'react';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  button: {
+    minWidth: '2.5rem',
+    maxWidth: '2.5rem',
+    height: '2.5rem',
+    background: '#FFFFFF',
+    color: '#00000094',
+    borderRadius: '50%',
+    boxShadow: 'none',
+    '&:active, &:hover': {
+      background: '#01579B',
+      color: '#FFFFFF',
+      border: '0.0625rem solid #82B1FF',
+    },
+  },
+});
+
+const LanguageButton = ({ children }: { children: ReactNode }) => {
+  const classes = useStyles();
+  return (
+    <Button className={classes.button} variant="contained">
+      {children}
+    </Button>
+  );
+};
+export default LanguageButton;
