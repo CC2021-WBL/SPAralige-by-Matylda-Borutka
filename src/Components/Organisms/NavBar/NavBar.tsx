34a98bc7e5c12665ac91ca99f7fdb7924e8dba28
Robert <elgreen@tlen.pl>
2022-04-11 00:19:46 +0200

    fix: updated colors

--- src/Components/Organisms/NavBar/NavBar.tsx
+++ src/Components/Organisms/NavBar/NavBar.tsx
@@ -2,8 +2,33 @@ import MenuIcon from '@mui/icons-material/Menu';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { AppBar, IconButton, Toolbar } from '@mui/material';
 import { Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { useState } from 'react';
+
+const useStyles = makeStyles({
+  open: {
+    transform: 'scale(1, 1)',
+    background: 'grey',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  closed: {
+    transform: 'scale(1, 0)',
+    position: 'absolute',
+  },
+});
 
 const NavBar = () => {
+  const classes = useStyles();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleHandler = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  // let domNode = useClickOutside(() => {
+  //   setMenuOpen(false);
+  // });
   return (
     <AppBar
       position="static"
@@ -14,22 +39,34 @@ const NavBar = () => {
           className="IconButton"
           edge="start"
           aria-label="menu"
-          sx={{ mr: 1, color: 'white' }}
+          sx={{ mr: 1, color: 'primary.contrastText' }}
         >
           <MenuIcon />
         </IconButton>
         <Typography
           variant="h6"
-          color="inherit"
+          color="primary.contrastText"
           component="div"
           textAlign="center"
           sx={{ width: '100%', userSelect: 'none' }}
         >
           SPARALIGE
         </Typography>
-        <IconButton sx={{ ml: 2, color: 'white' }}>
-          <MoreVertIcon />
-        </IconButton>
+        <div
+          onClick={toggleHandler}
+          onKeyPress={() => console.log('keyDown')}
+          role="button"
+          tabIndex={0}
+        >
+          <IconButton sx={{ ml: 2, color: 'primary.contrastText' }}>
+            <MoreVertIcon />
+          </IconButton>
+          <div className={menuOpen ? classes.open : classes.closed}>
+            <div>tutaj</div>
+            <div>bedzie</div>
+            <div>menu</div>
+          </div>
+        </div>
       </Toolbar>
     </AppBar>
   );
