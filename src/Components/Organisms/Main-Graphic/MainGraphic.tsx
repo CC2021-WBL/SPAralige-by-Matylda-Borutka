7cfdc03d9e1b6a03022c605d0331f23197068c08
Alicja Kondrat <pierwszazlewej@gmail.com>
2022-04-18 21:14:04 +0200

    - added wavy item

--- src/Components/Organisms/Main-Graphic/MainGraphic.tsx
+++ src/Components/Organisms/Main-Graphic/MainGraphic.tsx
@@ -3,11 +3,26 @@ import Button from '@mui/material/Button';
 import { Grid, Typography } from '@mui/material';
 
 import BannerLG from '../Main-Graphic/bannerLG.png';
+import Lines from '../../../Assets/Lines.png';
 
 function MainGraphic() {
   return (
     <Box>
       <Grid container height={'100%'}>
+        <Box position="absolute" marginTop={29}>
+          {' '}
+          <Box
+            component="img"
+            sx={{
+              marginTop: 3,
+
+              height: '100%',
+              width: '100%',
+            }}
+            alt="relaxed woman."
+            src={Lines}
+          />
+        </Box>
         <Grid item xs={12} md={7}>
           <Box
             component="img"
