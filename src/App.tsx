175891b93b9c29d3943ba4ad312133064e60fbb2
Tomasz <skretkowskitomasz@gmail.com>
2022-04-19 17:51:04 +0200

    SPAR-68 Error Fallback component, react-error-boundary library 	Added: react-error-boundary package and Error Fallback Component, 	       ErrorBoundary with fallback added to App.js, 	       buggy counter for testing purposes on TestingBody page, 	Removed: ErrorBoundary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import { Route, Routes } from 'react-router-dom';
 
 import AboutPage from './Components/PagesBody/AboutPage/AboutPage';
 import AccountPage from './Components/PagesBody/AccountPage/AccountPage';
+import ErrorFallback from './Components/PagesBody/ErrorFallback/ErrorFallback';
 import LandingPage from './Components/PagesBody/LandingPage/LandingPage';
 import Layout from './Components/Template/Layout/Layout';
 import ReservationsPage from './Components/PagesBody/ReservationsPage/ReservationsPage';
@@ -11,20 +13,22 @@ import TestingBody from './Components/PagesBody/TestingBody';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/test" element={<TestingBody />} />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/test" element={<TestingBody />} />
 
-          <Route path="/services" element={<ServicesPage />} />
+            <Route path="/services" element={<ServicesPage />} />
 
-          <Route path="/about" element={<AboutPage />} />
+            <Route path="/about" element={<AboutPage />} />
 
-          <Route path="/account" element={<AccountPage />} />
+            <Route path="/account" element={<AccountPage />} />
 
-          <Route path="/reservations" element={<ReservationsPage />} />
-        </Route>
-      </Routes>
+            <Route path="/reservations" element={<ReservationsPage />} />
+          </Route>
+        </Routes>
+      </ErrorBoundary>
     </div>
   );
 }
