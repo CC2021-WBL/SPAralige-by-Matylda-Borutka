64fc9b01ec7f0fbdd4fd24f2ae16d0ad0078762e
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-04-22 21:42:08 +0200

    SPAR-48 moved services logic from landing to services page

--- src/Components/PagesBody/ServicesPage/ServicesPage.tsx
+++ src/Components/PagesBody/ServicesPage/ServicesPage.tsx
@@ -1,7 +1,42 @@
+import { getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
+import ResponsiveGrid from '../../Template/Layout/ResponsiveGrid';
+import { serviceDataType } from '../../../Types/dbDataTypes';
+import { servicesRef } from '../../../Firebase/firebase';
+
 const ServicesPage = () => {
+  const [serviceObjectArray, setServiceObjectArray] = useState<
+    serviceDataType[] | null
+  >(null);
+
+  useEffect(() => {
+    const getServiceObjectArray = async () => {
+      try {
+        const snapshot = await getDocs(servicesRef);
+        if (!snapshot) {
+          throw new Error();
+        } else {
+          const serviceArray: serviceDataType[] = [];
+          snapshot.forEach((service) => {
+            const convertedData = service.data() as serviceDataType;
+            convertedData.id = service.id;
+            serviceArray.push(convertedData);
+          });
+          setServiceObjectArray(serviceArray);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getServiceObjectArray();
+  }, []);
+
   return (
     <main>
-      <h1>Tu będą informacje o zabiegach</h1>
+      {serviceObjectArray && (
+        <ResponsiveGrid serviceObjectArray={serviceObjectArray} />
+      )}
     </main>
   );
 };
