14660be45ae3597d8c93a82586d3f7e4a7e01e46
WeronikaBL <weronika@datenexperten.com>
2022-04-23 20:49:15 +0200

    test

--- src/Components/PagesBody/ServicesPage/ServicesPage.tsx
+++ src/Components/PagesBody/ServicesPage/ServicesPage.tsx
@@ -1,7 +1,7 @@
 import { getDocs } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
-import ResponsiveGrid from '../../Template/Layout/ResponsiveGrid';
+// import ResponsiveGrid from '../../Template/Layout/ResponsiveGrid';
 
 import { serviceDataType } from '../../../Types/dbDataTypes';
 import { servicesRef } from '../../../Firebase/firebase';
@@ -35,9 +35,9 @@ const ServicesPage = () => {
 
   return (
     <main>
-      {serviceObjectArray && (
+      {/* {serviceObjectArray && (
         <ResponsiveGrid serviceObjectArray={serviceObjectArray} />
-      )}
+      )} */}
     </main>
   );
 };
