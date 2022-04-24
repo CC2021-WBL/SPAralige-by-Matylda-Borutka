d7454981f7bcb603faed3864179e11590a52bd74
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-04-25 01:02:12 +0200

    test

---
+++ src/Components/Template/Layout/ResponsiveGrid.tsx
@@ -0,0 +1,25 @@
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+import ServiceCard from '../../Organisms/ServiceCard/ServiceCard';
+import { serviceDataType } from '../../../Types/dbDataTypes';
+
+export default function ResponsiveGrid(prop: {
+  serviceObjectArray: serviceDataType[];
+}) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 12, sm: 12, md: 12 }}
+      >
+        {Array.from(prop.serviceObjectArray).map((object, index) => (
+          <Grid item xs={12} sm={12} md={6} key={index}>
+            <ServiceCard serviceObject={object} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
