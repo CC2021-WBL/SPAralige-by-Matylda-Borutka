0962749cf03beb35d6755a510c886cbdb93b61df
Tomasz <skretkowskitomasz@gmail.com>
2022-04-25 13:30:42 +0200

    SPAR-85 Fetch fixes, alerts on errors, Loaders added

--- src/Components/PagesBody/ReservationsPage/ReservationsPage.tsx
+++ src/Components/PagesBody/ReservationsPage/ReservationsPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab, Tabs, Typography } from '@mui/material';
+import { Box, LinearProgress, Tab, Tabs, Typography } from '@mui/material';
 import { getDocs, orderBy, query, where } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
@@ -80,7 +80,7 @@ const ReservationsPage = () => {
           </Tabs>
           <TabPanel value={value} index={0}>
             <Box sx={reservationCardsBoxStyle}>
-              {pending && <h2>Loading...</h2>}
+              {pending && <LinearProgress />}
               {!pending &&
                 reservations &&
                 reservations.map((reservation, index) => (
